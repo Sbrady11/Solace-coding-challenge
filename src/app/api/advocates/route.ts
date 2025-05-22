adac19470bd0d26e6012ff469b55837a6477ff95
Simon Brady <simonbrady@Simons-MacBook-Pro.local>
2025-05-22 21:06:55 +0100

    feat: filter working so far

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,10 +1,24 @@
+import { ilike, or, and } from "drizzle-orm";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  const data = await db.select().from(advocates);
-  // const data = advocateData;
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const filter = searchParams.get("q");
+
+  const data = await db
+    .select()
+    .from(advocates)
+    .where(
+      and(
+        or(
+          ilike(advocates.firstName, `%${filter}%`),
+          ilike(advocates.lastName, `%${filter}%`),
+          ilike(advocates.city, `%${filter}%`),
+          ilike(advocates.degree, `%${filter}%`)
+        )
+      )
+    );
 
   return Response.json({ data });
 }
